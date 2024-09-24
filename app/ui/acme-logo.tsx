2d79b542fb0b9d6d1ea2aaa1a8a48ed71e9f266f
marble-ly <385516@naver.com>
2024-09-24 23:45:52 +0900

    서치바 추가 및 로고 변경

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,14 +1,13 @@
 //import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { BuildingLibraryIcon } from "@heroicons/react/24/outline";
-import { lusitana } from '@/app/ui/fonts';
+//import { BuildingLibraryIcon } from "@heroicons/react/24/outline";
+import { ubuntu } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-col items-start leading-none text-white`}
+      className={`${ubuntu.className} flex flex-col items-start leading-none text-black`}
     >
-      <BuildingLibraryIcon className="h-12 w-12" />
-      <p className="text-[44px]">SweatDeal</p>
+      <p className="text-[80px]">:Deal</p>
     </div>
   );
 }
--- app/ui/home/topnav.tsx
+++ app/ui/home/topnav.tsx
@@ -2,24 +2,29 @@ import Link from 'next/link';
 import NavLinks2 from '@/app/ui/home/nav-links2';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import SearchBar from './search-bar';
+import SearchPage from '@/app/home/search/page';
 
 export default function TopNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:flex-row md:px-2 md:space-x-2">
-      <Link className="mb-2 flex h-20 items-end justify-start rounded-md bg-rose-400 p-4 md:h-40" href="/">
+      <Link className="mb-2 flex h-20 items-end justify-start rounded-md bg-[#CDE8E5] p-4 md:h-40" href="/">
         <div className="w-32 text-white md:w-60">
           <AcmeLogo />
         </div>
       </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:items-end md:p-2 md:flex-row">
-        <NavLinks2 />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
+      <div>
+        <SearchPage />
+        <div className="flex grow flex-row justify-between space-x-2 md:items-end md:p-2 md:flex-row">
+          <NavLinks2 />
+          <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+          <form>
+            <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+              <PowerIcon className="w-6" />
+              <div className="hidden md:block ">Sign Out</div>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
