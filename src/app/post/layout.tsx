397066e0d36d0c8e874342b84a2d2ab175704bdc
marble-ly <385516@naver.com>
2024-11-02 01:42:31 +0900

    포스트 페이지 레이아웃 추가

---
+++ src/app/post/layout.tsx
@@ -0,0 +1,14 @@
+//import SideNav from '@/app/ui/home/sidenav';
+import TopNav from '@/src/app/ui/home/topnav';
+import DealLogo from '../ui/deal-logo';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:w-10/12 md:p-4 md:mx-auto md:border md:overflow-hidden"> {/* md:w-?/? 비율 설정해서 좌우 크기 조정 */}
+      <div className="w-full flex-none">
+        <TopNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-4 md:mx-80">{children}</div>
+    </div>
+  );
+}
---
+++ src/app/post/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import TableData from "@/src/app/ui/home/list/tabledata";
+import { Suspense } from "react";
+import { Spinner } from "@/src/app/ui/home/list/spinner";
+import "../globals.css";
+
+export default function Page() {
+    return (
+    <div>
+      this page is nothing
+    </div>
+    );
+  }
