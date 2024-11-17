ce2a29d90f9bb5759274b3b70521e7c96be6bc21
GyeongYunHeo <dongandn12@gmail.com>
2024-11-17 20:47:25 +0900

    검색 api 추가

--- server/server.js
+++ server/server.js
@@ -176,6 +176,23 @@ nextApp
       })
     })
 
+    
+    app.get('/*/search', (req, res) => {
+      //console.log(req.query.query);
+      // "search?query=" 뒤 url query값을 받아서 db에 검색 (검색값은 title 필드에 한해서 검색)
+      connection.query(`SELECT * FROM Post WHERE title LIKE ?`, '%' + req.query.query + '%', 
+        function(err, result) {
+        if (err) {
+          throw err;
+        } else {
+          console.log(result);
+          //res.send(result);  // db 검색 데이터 전달
+        }
+      })
+      return handle(req, res);
+    });
+    
+
     app.get('*', (req, res) => {
       return handle(req, res);
     });
