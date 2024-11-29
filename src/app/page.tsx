2b82ae5be083cb2d9105eb15a2c398f25676ea9d
marble-ly <wen091377@gmail.com>
2024-11-29 19:17:09 +0900

    로그인 창 한글화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,11 +25,11 @@ export default function Page() {
               <div className="space-y-4">
                 <form className="space-y-10 md:space-y-6" action="/auth/login_process" method='post'>
                   <div>
-                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">email or ID</label>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이메일 또는 아이디</label>
                     <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" />
                   </div>
                   <div>
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">비밀번호</label>
                     <input type="password" name="pwd" id="pwd" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                   </div>
                   <div className="flex items-center justify-between">
@@ -38,16 +38,16 @@ export default function Page() {
                         <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"/>
                       </div>
                       <div className="ml-3 text-sm">
-                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">아이디 저장</label>
                       </div>
                     </div>
-                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">비밀번호 찾기</a>
                   </div>
                   <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                     로그인
                   </button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                    Don't have an account yet? <a href="login/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
+                    <a href="login/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">회원가입</a>
                   </p>
                 </form>
 
