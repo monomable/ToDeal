593a29121b16789688cbe83066338c6bad0993de
marble-ly <wen091377@gmail.com>
2025-03-21 00:21:10 +0900

    아이콘 추가

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import Link from "next/link";
 
+import { UserIcon } from "@heroicons/react/24/outline";
+
 export default function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -15,11 +17,7 @@ export default function Navbar() {
         </Link>
 
         {/* 내비게이션 메뉴 */}
-        <div
-          className={`${
-            isMobileMenuOpen ? "block" : "hidden"
-          } absolute top-full left-0 w-full bg-white md:relative md:block md:w-auto md:left-1/2 md:-translate-x-1/2 md:bg-transparent`}
-        >
+        <div className={`${isMobileMenuOpen ? "block" : "hidden"} absolute top-full left-0 w-full bg-white md:relative md:block md:w-auto md:left-1/3 md:-translate-x-1/2 md:bg-transparent md:top-10`} >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link href="/" className="block py-2 px-3 text-white bg-blue-700 rounded-md md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent">
@@ -95,34 +93,63 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
-      {/* 모바일 메뉴 버튼 */}
-      <button
-          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden 
-                     hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 
-                     dark:hover:bg-gray-700 dark:focus:ring-gray-600 ml-auto"
-          aria-controls="navbar-dropdown"
-          aria-expanded={isMobileMenuOpen}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
+        {/* 모바일 메뉴 버튼 */}
+        <button
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden 
+                      hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 
+                      dark:hover:bg-gray-700 dark:focus:ring-gray-600 ml-auto"
+            aria-controls="navbar-dropdown"
+            aria-expanded={isMobileMenuOpen}
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        
+        {/* 검색창 */}
+        <form className="hidden md:block max-w-md ml-auto relative top-10">
+            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+              </div>
+              <input 
+                type="search" 
+                id="default-search" 
+                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 
+                           focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 
+                           dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search..."
+                required 
+              />
+            </div>
+          </form>
+
+          <Link href="/login" className="ml-10 relative top-10">
+            <UserIcon className="w-6 h-6 text-gray-700 dark:text-white cursor-pointer hover:text-blue-700 dark:hover:text-blue-500" />
+          </Link>
+
       </div>
+
+      <hr className="border-gray-200 dark:border-gray-700" />
+
     </nav>
   );
 }
